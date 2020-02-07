029fd0d16ed7b10074ff65160c0c58493c55727f
Manul228 <Manul228@users.noreply.github.com>
2020-02-07 11:43:08 +0300

    place 25 pictures

---
+++ js/main.js
@@ -0,0 +1,94 @@
+'use strict;'
+
+MESSAGES = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+];
+
+NAMES = [
+  'Артём',
+  'Вася',
+  'Пётр',
+  'Роберт Карлович',
+  'Вика',
+  'Маша',
+  'Сара'
+];
+
+AVATARS_AMOUNT = 6;
+PICTURES_AMOUNT = 25;
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+var getRandomElement = function (array) {
+  var len = array.length;
+
+  if (len === 0) {
+    return undefined;
+  }
+
+  return array[getRandomInt(0, len)];
+};
+
+var getPictures = function (amount, likesMinimum, likesMaximum) {
+  var pictures = []
+
+  for (var i = 1; i < amount + 1; ++i) {
+    var url = 'photos/' + i + '.jpg';
+    var description = 'Какое-то описание, в ТЗ нет указаний на этот счёт';
+    var likes = getRandomInt(likesMinimum, likesMaximum + 1);
+    var avatar = 'img/avatar-' + getRandomInt(1, AVATARS_AMOUNT + 1) + '.svg';
+    var message = getRandomElement(MESSAGES);
+    var name = getRandomElement(NAMES);
+
+    var picture = {
+      url: url,
+      description: description,
+      likes: likes,
+      comments: {
+        avatar: avatar,
+        message: message,
+        name: name
+      }
+    };
+    pictures.push(picture);
+  }
+  return pictures;
+};
+
+var placePictures = function (amount) {
+  var picturesContainer = document.querySelector('.pictures');
+
+  var pictureTemplate = document.querySelector('#picture').content;
+
+  var getPictureElement = function (picture) {
+    var pictureElement = pictureTemplate.querySelector('.picture').cloneNode(true);
+
+    pictureElement.querySelector('.picture__img').src = picture.url;
+    pictureElement.querySelector('.picture__likes').textContent = picture.likes;
+    pictureElement.querySelector('.picture__comments').textContent = (picture.comments).toString();
+
+    return pictureElement;
+  };
+
+  var pictures = getPictures(amount, 15, 200);
+
+  var fragment = document.createDocumentFragment();
+
+  for (var i = 0; i < amount; ++i) {
+    fragment.appendChild(getPictureElement(pictures[i]), pictureTemplate);
+  }
+
+  picturesContainer.appendChild(fragment);
+};
+
+placePictures(25);
+
